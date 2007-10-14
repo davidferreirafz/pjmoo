34f55caa8761ec30c0669f7fc28a8372bcd5448f
David Ferreira <davidferreira.fz@gmail.com>
2007-10-14 15:33:46 +0000

    Renomeando algumas classes, para facilitar entendimento;

--- projetos/games/pancada/src/StatusLutador.h
+++ projetos/games/pancada/src/StatusLutador.h
@@ -1,19 +1,38 @@
+//***************************************************************************
+//  Pancada <Game - Boxing Style>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.sourceforge.net 
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da 
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or 
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,                                       
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
+#ifndef _STATUSLUTADOR_H
+#define _STATUSLUTADOR_H
 
-#ifndef _HUDLUTADOR_H
-#define _HUDLUTADOR_H
-
-#include "HUDAbstract.h"
+#include "Status.h"
 #include <GBF/SpriteItem.h>
 
 
-class HUDLutador : public HUDAbstract
+class StatusLutador : public Status
 {
   public:
     //Construtor
-    HUDLutador();
+    StatusLutador();
 
     //Destrutor
-    virtual ~HUDLutador();
+    virtual ~StatusLutador();
 
     virtual void desenhar(int pontos, int round, int energia) = 0;
 
--- projetos/games/pancada/src/StatusPlayer.h
+++ projetos/games/pancada/src/StatusPlayer.h
@@ -1,10 +1,29 @@
-
+//***************************************************************************
+//  Pancada <Game - Boxing Style>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.sourceforge.net 
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da 
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or 
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,                                       
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
 #ifndef _STATUSPLAYER_H
 #define _STATUSPLAYER_H
 
-#include "HUDLutador.h"
+#include "StatusLutador.h"
 
-class StatusPlayer : public HUDLutador
+class StatusPlayer : public StatusLutador
 {
   public:
     //Construtor

a992d986524206d5462e09c8d4dbf50d9c633508
F-Z <F-Z@f3efac32-a332-0410-b381-edff871ac65d>
2007-10-14 15:33:46 +0000

    Renomeando algumas classes, para facilitar entendimento;

--- projetos/games/pancada/src/FaseAbstract.h
+++ projetos/games/pancada/src/FaseAbstract.h
@@ -1,11 +1,30 @@
-
+//***************************************************************************
+//  Pancada <Game - Boxing Style>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.sourceforge.net 
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da 
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or 
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,                                       
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
 #ifndef _FASEABSTRACT_H
 #define _FASEABSTRACT_H
 
 #include "LutadorAbstract.h"
 #include "Placar.h"
 #include "Round.h"
-#include "HUDTempo.h"
+#include "StatusTempo.h"
 #include "StatusPC.h"
 #include "StatusPlayer.h"
 #include <GBF/InputSystem.h>
@@ -36,7 +55,7 @@ class FaseAbstract
 
     Round round;
 
-    HUDTempo statusTempo;
+    StatusTempo statusTempo;
 
     StatusPC statusPC;
 
--- projetos/games/pancada/src/StatusTempo.h
+++ projetos/games/pancada/src/StatusTempo.h
@@ -1,15 +1,35 @@
+//***************************************************************************
+//  Pancada <Game - Boxing Style>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.sourceforge.net 
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da 
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or 
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,                                       
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
+#ifndef _STATUSTEMPO_H
+#define _STATUSTEMPO_H
 
-#ifndef _HUDTEMPO_H
-#define _HUDTEMPO_H
+#include "Status.h"
 
-#include "HUDAbstract.h"
-
-class HUDTempo : public HUDAbstract
+//Informações sobre o tempo do jogo
+class StatusTempo : public Status
 {
   public:
-    HUDTempo();
+    StatusTempo();
 
-    virtual ~HUDTempo();
+    virtual ~StatusTempo();
 
     void desenhar(int tempo);
 

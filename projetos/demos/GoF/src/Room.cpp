d243ccfb4bacbcf2dea6bb28056d0a3b8d1ff03b
David Ferreira <davidferreira.fz@gmail.com>
2007-11-25 02:42:47 +0000

    Adicionando exemplo base do GoF

---
+++ projetos/demos/GoF/src/Room.cpp
@@ -0,0 +1,100 @@
+//***************************************************************************
+//  UML <Demo - GoF - Designer Patterns>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.wiki.sourceforge.net/GoF
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da 
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or 
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,                                       
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
+#include "Room.h"
+#include "Door.h"
+
+Room::Room(int number) 
+{
+    roomNumber = number;
+
+    SpriteFactory  *spriteFactory = new SpriteFactory(graphicSystem->imageBufferManager->getImageBuffer("tiles"));
+    sprite = spriteFactory->criarSpriteItem(32,0,32,32,1,1);
+
+    delete(spriteFactory);
+}
+Room::~Room() 
+{
+    for (int i=0; i<4; i++){
+        if (sides[i]){
+            if (sides[i]->isInstance<Door>()){
+                Door * door = (Door *)sides[i];
+                Room * room = door->otherSideFrom(this);
+                if (room){
+                    Direction direction = room->getDirection(door);
+                    if (direction!=INVALID){
+                        room->setSide(direction,NULL);
+                    }
+                }
+            }
+            delete(sides[i]);
+        }
+    }
+}
+void Room::enter() 
+{
+}
+void Room::draw(int x, int y) 
+{
+    if (sprite){
+        sprite->desenhar(x,y);
+    }
+
+    if (sides[North]){
+        sides[North]->draw(x,y-32);
+    }
+
+    if (sides[East]){
+        sides[East]->draw(x+32,y);
+    }
+
+    if (sides[South]){
+        sides[South]->draw(x,y+32);
+    }
+
+    if (sides[West]){
+        sides[West]->draw(x-32,y);
+    }
+}
+void Room::setSide(Direction direction, MapSite * map) 
+{
+    sides[direction]=map;
+}
+MapSite * Room::getSide(Direction direction) const 
+{
+    return sides[direction];
+}
+Direction Room::getDirection(MapSite * map) 
+{
+    Direction direction = INVALID;
+
+    for (int i=0; i<4; i++){
+        if (sides[i]==map){
+            direction = (Direction) i;
+            break;
+        }
+    }
+
+    return direction;
+}
+int Room::getNumber() 
+{
+    return roomNumber;
+}

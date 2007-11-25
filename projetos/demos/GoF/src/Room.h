e325e5b1c467b5f4f1990dda5f9a2bea2014a82e
F-Z <F-Z@f3efac32-a332-0410-b381-edff871ac65d>
2007-11-25 02:42:47 +0000

    Adicionando exemplo base do GoF

---
+++ projetos/demos/GoF/src/Room.h
@@ -0,0 +1,54 @@
+//***************************************************************************
+//  UML <Demo - GoF - Designer Patterns>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.wiki.sourceforge.net/GoF
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da 
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or 
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,                                       
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
+#ifndef _ROOM_H
+#define _ROOM_H
+
+#include "MapSite.h"
+
+class Door;
+
+class Room : public MapSite
+{
+  public:
+    Room(int number);
+
+    virtual ~Room();
+
+    virtual void enter();
+
+    virtual void draw(int x, int y);
+
+    void setSide(Direction direction, MapSite * map);
+
+    MapSite * getSide(Direction direction) const;
+
+    Direction getDirection(MapSite * map);
+
+    int getNumber();
+
+
+  protected:
+    int roomNumber;
+
+    MapSite * sides[4];
+
+};
+#endif
---
+++ projetos/demos/GoF/src/Wall.cpp
@@ -0,0 +1,36 @@
+//***************************************************************************
+//  UML <Demo - GoF - Designer Patterns>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.wiki.sourceforge.net/GoF
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da 
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or 
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,                                       
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
+#include "Wall.h"
+
+Wall::Wall() 
+{
+    SpriteFactory  *spriteFactory = new SpriteFactory(graphicSystem->imageBufferManager->getImageBuffer("tiles"));
+    sprite = spriteFactory->criarSpriteItem(0,0,32,32,1,1);
+
+    delete(spriteFactory);
+}
+Wall::~Wall() 
+{
+
+}
+void Wall::enter() 
+{
+}

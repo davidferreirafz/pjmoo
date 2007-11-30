9caaa7e1fd13e5b9d1c526c138171bbeefb4711e
F-Z <F-Z@f3efac32-a332-0410-b381-edff871ac65d>
2007-11-30 01:49:06 +0000

    Atualizando GoF Protoype

---
+++ projetos/demos/GoF_Prototype/src/Maze.h
@@ -0,0 +1,48 @@
+//***************************************************************************
+//  UML <Demo - GoF - Designer Patterns>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.wiki.sourceforge.net/GoF
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da 
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or 
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,                                       
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
+#ifndef _MAZE_H
+#define _MAZE_H
+
+#include "Room.h"
+
+class Maze
+{
+  public:
+    Maze();
+
+    Maze(const Maze & source);
+
+    virtual ~Maze();
+
+    void addRoom(Room * room, int column, int line);
+
+    Room * roomNo(int number) const;
+
+    void draw();
+
+    Maze * clone();
+
+
+  private:
+    Room * rooms[19][14];
+
+};
+#endif

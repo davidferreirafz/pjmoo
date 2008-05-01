c1b8fac64322cffdafc3e43273ff6ea55d8db2cd
F-Z <F-Z@f3efac32-a332-0410-b381-edff871ac65d>
2008-05-01 16:47:43 +0000

    Adicionando Demo para implementação de TecladoVirtual

---
+++ projetos/demos/TecladoVirtual/src/UserInterfaceNovoRecorde.cpp
@@ -0,0 +1,305 @@
+
+#include "UserInterfaceNovoRecorde.h"
+
+UserInterfaceNovoRecorde::UserInterfaceNovoRecorde()
+{
+    //Para não esquecer:
+    //As Soluções mais simples, são as que melhor funcionam :P
+    strcpy(miniTeclado,"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 _+-$%*,.:[]!=");
+
+    tecladoControle[0]="GBF_UIRecorde_button_back";
+    tecladoControle[1]="GBF_UIRecorde_button_next";
+    tecladoControle[2]="GBF_UIRecorde_button_save";
+
+    totalTeclasTeclado  = strlen(miniTeclado);
+    totalTeclasControle = 3;
+
+    totalTeclas    = totalTeclasTeclado+totalTeclasControle;
+    nomePosicao    = 0;
+    tecladoSelecao = 0;
+    posicao.x      = 0;
+    posicao.y      = 0;
+
+    tempoNavegacao.setTempoOriginal(1);
+    tempoNavegacao.setUnidade(TEMPO_CENTESIMO);
+    tempoNavegacao.setResetar();
+
+    tempoBlink.setTempoOriginal(0);
+    tempoBlink.setUnidade(TEMPO_CENTESIMO);
+    tempoBlink.setResetar();
+}
+UserInterfaceNovoRecorde::~UserInterfaceNovoRecorde()
+{
+//não implementado
+}
+//Desenha o botão de ação da janela
+void UserInterfaceNovoRecorde::desenharBotao()
+{
+    graphicSystem->gfx->setColor(255,255,0);
+    int letra=0;
+    int espacoHorizontal = dimensaoFonteTeclado.w + int(dimensaoFonteTeclado.w / 4);
+    int espacoVertical   = dimensaoFonteTeclado.h + int(dimensaoFonteTeclado.h / 4);
+
+    Ponto tecla;
+    tecla.x=caixaPosicao.x + (dimensaoFonteTeclado.w/4);
+    tecla.y=caixaPosicao.y;
+
+    Ponto cursor;
+    cursor.x=caixaPosicao.x + (dimensaoFonteTeclado.w * 0.2);
+    cursor.y=caixaPosicao.y + (dimensaoFonteTeclado.h * 0.1);
+
+    Dimensao cursorDimensao;
+    cursorDimensao.w=dimensaoFonteTeclado.w;
+    cursorDimensao.h=dimensaoFonteTeclado.h;
+
+    //Painel do teclado
+    for (int l=0;l<5;l++){
+        for (int c=0;c<10;c++){
+            wsManager->escrever(fonteTeclado, tecla.x +(espacoHorizontal*c),tecla.y+(espacoVertical*l),"%c",miniTeclado[letra]);
+
+            //Desenhando cursor da selecao de tecla
+            if ((tecladoSelecao==letra)&&(tempoBlink.getTempo()%2==0)){
+                graphicSystem->gfx->retangulo(cursor.x+(espacoHorizontal*c),cursor.y+(espacoVertical*l),cursorDimensao.w,cursorDimensao.h);
+            }
+            letra++;
+        }
+    }
+
+    tecla.x=caixaPosicao.x + (10 * (dimensaoFonteTeclado.w + dimensaoFonteTeclado.w*0.3));
+    tecla.y=caixaPosicao.y + caixaTeclado.h - (totalTeclasControle * dimensaoFonteControle.h);
+
+    //Painel das teclas de controles
+    for (int ic=totalTeclasControle-1;ic>=0;ic--){
+        wsManager->escreverLocalizado(fonteControle, tecla.x,tecla.y+(dimensaoFonteControle.h*ic),tecladoControle[ic].c_str());
+    }
+
+    cursor.x=tecla.x - int(dimensaoFonteControle.w*0.25);
+    cursor.y=tecla.y;
+
+    cursorDimensao.w=tamanhoMaiorTeclaControle + (dimensaoFonteControle.w*0.5);
+    cursorDimensao.h=dimensaoFonteControle.h;
+
+    //Desenhando cursor das teclas de controle
+    if (tempoBlink.getTempo()%2==0){
+        if (tecladoSelecao>=totalTeclasTeclado){
+            int t=tecladoSelecao-totalTeclasTeclado;
+            graphicSystem->gfx->retangulo(cursor.x,cursor.y+(dimensaoFonteControle.h*t),cursorDimensao.w,cursorDimensao.h);
+        }
+    }
+}
+void UserInterfaceNovoRecorde::desenharBackground()
+{
+    UserInterfaceWindow::desenharBackground();
+
+    int auxLargura=caixaTeclado.w+tamanhoMaiorTeclaControle+(dimensaoFonteLabel.w);
+
+    graphicSystem->gfx->setColor(0,0,0);
+    graphicSystem->gfx->retanguloPreenchido(caixaPosicao.x,caixaPosicao.y,auxLargura,caixaTeclado.h);
+    graphicSystem->gfx->setColor(206,101,99);
+    graphicSystem->gfx->retangulo(caixaPosicao.x,caixaPosicao.y,auxLargura,caixaTeclado.h);
+}
+
+
+//Desenha o conteudo da janela
+void UserInterfaceNovoRecorde::desenharConteudo()
+{
+    int posicaoTextoHorizontal = 0;
+    int posicaoTextoVertical   = posicao.y;
+
+    //escrevendo titulo centralizado
+    int media = wsManager->getLarguraLinha(fonteTitulo,"GBF_UIRecorde_title");
+    posicaoTextoHorizontal=int (posicao.x+(dimensao.w/2)-(media/2));
+
+    wsManager->escreverLocalizado(fonteTitulo,posicaoTextoHorizontal,posicaoTextoVertical,"GBF_UIRecorde_title");
+
+    //escrevendo label
+    posicaoTextoVertical=posicaoTextoVertical+(dimensaoFonteTitulo.h*1.5);
+
+    int auxPlayerX = posicao.x+dimensaoFonteLabel.w*2;
+
+    int auxPointsX = wsManager->getLarguraLinha(fonteLabel,"GBF_UIRecorde_title_points");
+    auxPointsX=int (posicao.x+(dimensao.w/2)) + dimensaoFonteLabel.w*2;
+
+    wsManager->escreverLocalizado(fonteLabel, auxPlayerX,posicaoTextoVertical,"GBF_UIRecorde_title_player");
+    wsManager->escreverLocalizado(fonteLabel, auxPointsX,posicaoTextoVertical,"GBF_UIRecorde_title_points");
+
+    //escrevendo campo
+    posicaoTextoVertical=posicaoTextoVertical+(dimensaoFonteLabel.h*1.5);
+
+    auxPlayerX-=dimensaoFonteCampo.w;
+    auxPointsX-=dimensaoFonteCampo.w;
+
+    wsManager->escrever(fonteCampo, auxPlayerX, posicaoTextoVertical,"%s"  ,recorde.nome);
+    wsManager->escrever(fonteCampo, auxPointsX, posicaoTextoVertical,"%08d",recorde.pontos);
+
+    //desenhando cursor
+    if (tempoBlink.getTempo()%2!=0){
+        graphicSystem->gfx->setColor(250,250,250);
+        graphicSystem->gfx->retanguloPreenchido(auxPlayerX+(dimensaoFonteCampo.w*nomePosicao),posicaoTextoVertical+dimensaoFonteCampo.h,dimensaoFonteCampo.w,2);
+    }
+}
+
+//Inicializa as configurações da caixa de texto
+void UserInterfaceNovoRecorde::inicializar()
+{
+    UserInterfaceWindow::inicializar();
+}
+
+////////////////////////////////////////////////
+
+//Define a fonte a ser usada pelo teclado virtual
+void UserInterfaceNovoRecorde::setFonteTeclado(std::string fonte)
+{
+    fonteTeclado=fonte;
+    dimensaoFonteTeclado=wsManager->getFonte(fonteTeclado)->getDimensao();
+
+    caixaTeclado.w=10 * (dimensaoFonteTeclado.w + int(dimensaoFonteTeclado.w/4));
+    caixaTeclado.h=5  * (dimensaoFonteTeclado.h + int(dimensaoFonteTeclado.h/4));
+
+    caixaPosicao.x=posicao.x + (dimensaoFonteTeclado.h*1.50);
+    caixaPosicao.y=posicao.y+dimensao.h-caixaTeclado.h-dimensaoFonteTeclado.h*1.50;
+}
+
+void UserInterfaceNovoRecorde::setFonteTitulo(std::string fonte)
+{
+    fonteTitulo=fonte;
+    dimensaoFonteTitulo=wsManager->getFonte(fonteTitulo)->getDimensao();
+}
+//Define a fonte a ser usada pelo label
+void UserInterfaceNovoRecorde::setFonteLabel(std::string fonte)
+{
+    fonteLabel=fonte;
+    dimensaoFonteLabel=wsManager->getFonte(fonteLabel)->getDimensao();
+}
+void UserInterfaceNovoRecorde::setFonteCampo(std::string fonte)
+{
+    fonteCampo=fonte;
+    dimensaoFonteCampo=wsManager->getFonte(fonteCampo)->getDimensao();
+}
+//Define a fonte a ser usada pelo label
+void UserInterfaceNovoRecorde::setFonteControle(std::string fonte)
+{
+    fonteControle=fonte;
+    dimensaoFonteControle=wsManager->getFonte(fonteControle)->getDimensao();
+
+    tamanhoMaiorTeclaControle  = 0;
+    for (unsigned int i=0; i<totalTeclasControle;i++){
+        if (tecladoControle[i].length()>tamanhoMaiorTeclaControle){
+            tamanhoMaiorTeclaControle=wsManager->getLarguraLinha(fonteControle,tecladoControle[i].c_str());
+        }
+    }
+}
+
+//Retorna o TopSystemRecorde
+TopSystemRecorde UserInterfaceNovoRecorde::getRecorde()
+{
+    return recorde;
+}
+//Atribui um TopSystemRecorde para complementar os dados
+void UserInterfaceNovoRecorde::setRecorde(TopSystemRecorde recorde)
+{
+    this->recorde  = recorde;
+    nomePosicao    = 0;
+    tecladoSelecao = 0;
+    tempoBlink.setResetar();
+    tempoNavegacao.setResetar();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Gerencia o controle do cursor (navegação) e as opções selecionadas
+bool UserInterfaceNovoRecorde::controle()
+{
+    bool salva = false;
+    tempoNavegacao.processar();
+    tempoBlink.processar();
+    if (tempoNavegacao.isTerminou()){
+        navegar();
+        salva = confirmar();
+    }
+    return salva;
+}
+
+//Efetua as ações de acordo com a posição do cursor
+bool UserInterfaceNovoRecorde::confirmar()
+{
+    bool salva = false;
+    if ((inputSystem->teclado->isKey(SDLK_RETURN)) || (inputSystem->joystick->isButtonA())){
+        if ((tecladoSelecao>=0)&&(tecladoSelecao<totalTeclasTeclado)){
+            recorde.nome[nomePosicao]=miniTeclado[tecladoSelecao];
+            nomePosicao++;
+        } else if (tecladoSelecao==totalTeclasTeclado){//controle voltar
+            nomePosicao--;
+        } else if (tecladoSelecao==totalTeclasTeclado+1){//controle avancar
+            nomePosicao++;
+        } else if (tecladoSelecao==totalTeclasTeclado+2){//controle salvar
+            if (recorde.nome[0]!=' '){
+                salva=true;
+            }
+        }
+        tempoNavegacao.setResetar();
+    }
+
+    if (nomePosicao<0){
+        nomePosicao=0;
+    } else if (nomePosicao>9){
+        nomePosicao=9;
+    }
+
+    return salva;
+}
+//Efetua o controle sobre a navegação do cursor
+void UserInterfaceNovoRecorde::navegar()
+{
+    if ((inputSystem->teclado->isKey(SDLK_LEFT))||(inputSystem->joystick->isAxeLeft())){
+            tecladoSelecao--;
+            tempoNavegacao.setResetar();
+    } else if ((inputSystem->teclado->isKey(SDLK_RIGHT))||(inputSystem->joystick->isAxeRight())){
+            tecladoSelecao++;
+            tempoNavegacao.setResetar();
+    } else if ((inputSystem->teclado->isKey(SDLK_DOWN))||(inputSystem->joystick->isAxeDown())){
+            if (tecladoSelecao<40){
+                tecladoSelecao+=10;
+            } else {//if (tecladoSelecao>=totalTeclasTeclado){
+                tecladoSelecao++;
+            }
+            tempoNavegacao.setResetar();
+    } else if ((inputSystem->teclado->isKey(SDLK_UP))||(inputSystem->joystick->isAxeUp())){
+            if ((tecladoSelecao>=10)&&(tecladoSelecao<totalTeclasTeclado)){
+                tecladoSelecao-=10;
+            } else {
+                tecladoSelecao--;
+            }
+            tempoNavegacao.setResetar();
+    }
+
+    if (tecladoSelecao<0){
+        tecladoSelecao=0;
+    } else if (tecladoSelecao>totalTeclas-1){
+        tecladoSelecao=totalTeclas-1;
+    }
+}
+
---
+++ projetos/demos/TecladoVirtual/src/main.cpp
@@ -0,0 +1,119 @@
+//***************************************************************************
+//  UML <Demo - GoF - Designer Patterns>
+//  Copyright (C) 2007 by David Ferreira - FZ
+//  davidferreira.fz@gmail.com - http://pjmoo.wiki.sourceforge.net/GoF
+//***************************************************************************
+//    Este programa é software livre; você pode redistribuí-lo e/ou
+//    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+//    publicada pela Free Software Foundation; tanto a versão 2 da
+//    Licença como (a seu critério) qualquer versão mais nova.
+//***************************************************************************
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the
+//    Free Software Foundation, Inc.,
+//    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//***************************************************************************
+#include "main.h"
+
+
+int main(int argc, char * argv[])
+{
+    GBF frameworkGBF;
+
+    frameworkGBF.setPath(argv[0]);
+    frameworkGBF.setTitulo("TecladoVirtual","David Ferreira");
+    frameworkGBF.iniciar(640,480,16,false);
+
+//carregando imagens
+    frameworkGBF.graphicSystemCore->graphicSystem->imageBufferManager->carregar("tiles","//data//imagem//tiles.png");
+
+    frameworkGBF.writeSystem->carregar("nisemegaeu",frameworkGBF.getPath()+"data//fonte//nisemegaeu.png");
+    frameworkGBF.writeSystem->carregar("texto",frameworkGBF.getPath()+"data//fonte//ds9_computer.png");
+    frameworkGBF.writeSystem->carregar("recorde",frameworkGBF.getPath()+"data//fonte//recorde.png");
+    frameworkGBF.writeSystem->carregar("nisemega_extra",frameworkGBF.getPath()+"data//fonte//nisemegaeu.png");
+
+    frameworkGBF.writeSystem->getFonte("recorde")->setDimensao(24,24);
+
+
+//configurando suporte a idiomas
+    frameworkGBF.writeSystem->uiTexto->setArquivo("msg.txt");
+    frameworkGBF.writeSystem->uiTexto->detectarIdioma();
+
+
+    UserInterfaceVisualSolido *uiVisualImagem = new UserInterfaceVisualSolido();
+    uiVisualImagem->setCorBorda(255,255,0);
+    uiVisualImagem->setCorFundo(0,0,0);
+
+    UserInterfaceVisualImagem *uiVisualTransparente = new UserInterfaceVisualImagem();
+    uiVisualTransparente->setCorBorda(255,255,0);
+    uiVisualTransparente->setTipoBackground(BAKCGROUND_LINES_BLACK);
+
+
+
+    TopSystemRecorde recorde;
+    recorde.inicializar();
+    strcpy(recorde.nome,"          \0");
+    recorde.pontos  = 0;
+    recorde.fase    = 0;
+
+
+    UserInterfaceNovoRecorde * janelaRecordeNovo;
+
+    janelaRecordeNovo = new UserInterfaceNovoRecorde();
+    janelaRecordeNovo->setPosicao(100,100);
+    janelaRecordeNovo->setDimensao(500,330);
+//    janelaRecordeNovo->titulo.setFonte("texto");
+//    janelaRecordeNovo->titulo.setChaveTexto("tela_top_recorde_1");
+    janelaRecordeNovo->setVisual(uiVisualTransparente->clone());
+
+
+    janelaRecordeNovo->setFonteTitulo("recorde");
+    janelaRecordeNovo->setFonteLabel("texto");
+    janelaRecordeNovo->setFonteCampo("nisemega_extra");
+    janelaRecordeNovo->setFonteTeclado("recorde");
+    janelaRecordeNovo->setFonteControle("texto");
+
+    janelaRecordeNovo->setRecorde(recorde);
+    janelaRecordeNovo->inicializar();
+
+    delete(uiVisualImagem);
+    delete(uiVisualTransparente);
+
+
+
+/*
+    uiRecordeNovo = new UserInterfaceRecorde();
+    uiRecordeNovo->setWriteManager(frameworkGBF->writeSystem);
+    uiRecordeNovo->setGFX(frameworkGBF->graphicSystemCore->graphicSystem->gfx);
+    uiRecordeNovo->setFonteLabel("texto");
+    uiRecordeNovo->setFonteTeclado("recorde");
+    uiRecordeNovo->setPosicao(80,130);
+*/
+    while(true) {
+
+        if (frameworkGBF.inputSystemCore->inputSystem->teclado->isKey(SDLK_ESCAPE)){
+            break;
+        }
+
+
+        janelaRecordeNovo->executar();
+
+        if (janelaRecordeNovo->controle()){
+//            setTopGaleriaSalvar();
+        }
+
+        //realiza refresh, fps, flip
+        frameworkGBF.atualizar();
+        frameworkGBF.graphicSystemCore->clear();
+        frameworkGBF.graphicSystemCore->graphicSystem->gfx->circuloPreenchido(300,200,200);
+    }
+
+    delete(janelaRecordeNovo);
+
+    return 0;
+}

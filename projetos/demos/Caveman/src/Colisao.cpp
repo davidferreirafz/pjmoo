1e892edfdce3140cfc99079d347074ebe59c4497
David Ferreira <davidferreira.fz@gmail.com>
2008-06-15 18:18:40 +0000

    Caveman:: Atualizando demo tech - Plataforma

--- projetos/demos/Caveman/src/Colisao.cpp
+++ projetos/demos/Caveman/src/Colisao.cpp
@@ -17,8 +17,9 @@ bool Colisao::box(Regiao a, Regiao b)
 
 void Colisao::cenario(PersonagemAdventure * personagem, GBF::Imagem::Layer::FrameLayer * layer)
 {
-    GBF::Dimensao tile; tile.w = 32; tile.h = 32;
+    GBF::Dimensao tile = layer->mundo.getPixelTile();
     GBF::Dimensao tela; tela.w = 20; tela.h = 15;
+
     int indice = 0;
 
     Regiao regiaoTile;
@@ -37,9 +38,9 @@ void Colisao::cenario(PersonagemAdventure * personagem, GBF::Imagem::Layer::Fram
     //celulaFinal.x = ((regiao.posicao.x+regiao.dimensao.w)/tile.w);
     //celulaFinal.y = ((regiao.posicao.y+regiao.dimensao.h)/tile.h);
 
-    std::cout << "--- ---" <<std::endl;
-    std::cout << " Personagem (E):"<< regiao.posicao.x <<","<< regiao.posicao.y << " cel(I): "<< celulaInicial.x <<"," << celulaInicial.y << " cel(F): "<< celulaFinal.x <<"," << celulaFinal.y <<std::endl;
-    std::cout << "--- ---" <<std::endl;
+    //std::cout << "--- ---" <<std::endl;
+    //std::cout << " Personagem (E):"<< regiao.posicao.x <<","<< regiao.posicao.y << " cel(I): "<< celulaInicial.x <<"," << celulaInicial.y << " cel(F): "<< celulaFinal.x <<"," << celulaFinal.y <<std::endl;
+    //std::cout << "--- ---" <<std::endl;
 
     if ((celulaInicial.x >=0)&&(celulaInicial.y >=0)&&(celulaFinal.x<tela.w)&&(celulaFinal.y<tela.h)){
 
@@ -49,7 +50,7 @@ void Colisao::cenario(PersonagemAdventure * personagem, GBF::Imagem::Layer::Fram
                 indice = (coluna < tela.w) ? coluna : coluna - tela.w;
                 indice+= (linha < tela.h) ? linha * tela.w : (linha - tela.h) * tela.w;
 
-                std::cout << " | " <<indice;
+                //std::cout << " | " <<indice;
 
                 if (layer->getTipoImagem(indice)>0){
                     regiaoTile.posicao.x = coluna * tile.w;
@@ -66,7 +67,7 @@ void Colisao::cenario(PersonagemAdventure * personagem, GBF::Imagem::Layer::Fram
                             //evitar descer dos bricks - verificar
                             (regiao.posicao.y+regiao.dimensao.h>=regiaoTile.posicao.y)){
                             //(regiao.posicao.y>=regiaoTile.posicao.y+(regiaoTile.dimensao.h*0.5))){
-                                std::cout << " Colisao- TET: " << indice;
+                                //std::cout << " Colisao- TET: " << indice;
                                 regiao.posicao.y=regiaoTile.posicao.y+regiaoTile.dimensao.h;
                             }
                         } else if ((linha==celulaFinal.y)&&(coluna==int(xm/32))){
@@ -74,20 +75,20 @@ void Colisao::cenario(PersonagemAdventure * personagem, GBF::Imagem::Layer::Fram
                                 //evitar subir em bricks maior que a cintura do personagem
                                 (regiao.posicao.y+(regiao.dimensao.h*0.5)<=regiaoTile.posicao.y+(regiaoTile.dimensao.h*0.5))
                                 ){
-                                std::cout << " Colisao- CHA: " << indice;
+                                //std::cout << " Colisao- CHA: " << indice;
                                 regiao.posicao.y=regiaoTile.posicao.y-regiao.dimensao.h;
                                 personagem->setParouCair();
                             }
                         } else if(linha==celulaFinal.y-1){
                             if ((regiao.posicao.x<=regiaoTile.posicao.x+regiaoTile.dimensao.w)&&
                                 (regiao.posicao.x>=regiaoTile.posicao.x+(regiaoTile.dimensao.w*0.5))){
-                                    std::cout << " Colisao- ESQ: " << indice;
+                                    //std::cout << " Colisao- ESQ: " << indice;
                                     regiao.posicao.x=regiaoTile.posicao.x+regiaoTile.dimensao.w;
                             } else
                             //colisao direita
                             if ((regiao.posicao.x+regiao.dimensao.w>=regiaoTile.posicao.x)&&
                                 (regiao.posicao.x+regiao.dimensao.w<=regiaoTile.posicao.x+(regiaoTile.dimensao.w*0.5))){
-                                    std::cout << " Colisao- DIR: " << indice;
+                                    //std::cout << " Colisao- DIR: " << indice;
                                     regiao.posicao.x=regiaoTile.posicao.x-regiao.dimensao.w;
                                 /*int tipo=layer->getTipoColisao(indice);
                                 if (tipo==2){
@@ -109,8 +110,8 @@ void Colisao::cenario(PersonagemAdventure * personagem, GBF::Imagem::Layer::Fram
     }
 
     personagem->setPosicao(regiao.posicao.x,regiao.posicao.y);
-    std::cout << " "<<std::endl;
-    std::cout << " Personagem (S):"<< regiao.posicao.x <<","<< regiao.posicao.y<<std::endl;
+    //std::cout << " "<<std::endl;
+    //std::cout << " Personagem (S):"<< regiao.posicao.x <<","<< regiao.posicao.y<<std::endl;
 }
 
 

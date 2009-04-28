f6e3249c0d36eca21fb512fbeee7fc2e3e757d88
F-Z <F-Z@f3efac32-a332-0410-b381-edff871ac65d>
2009-04-28 16:01:50 +0000

    Gameta_SalaTreino : Incluindo demo para o projeto GAMETA, primeiros testes da sala de treino.

---
+++ projetos/demos/Gameta_SalaTreino/src/Canhao.cpp
@@ -0,0 +1,54 @@
+#include "Canhao.h"
+
+Canhao::Canhao(int x, int y)
+{
+    GBF::Imagem::SpriteFactory *spriteFactory = new GBF::Imagem::SpriteFactory("fumaca");
+    fumaca=spriteFactory->criarSpriteItem(0,0,69,63,7,1);
+    fumaca->animacao.setAutomatico(true);
+    delete(spriteFactory);
+
+    spriteFactory = new GBF::Imagem::SpriteFactory("bala");
+    bala=spriteFactory->criarSpriteItem(0,0,24,20,0,0);
+    bala->animacao.setAutomatico(true);
+    delete(spriteFactory);
+
+    ponto.x=x;
+    ponto.y=y;
+    xBala=x-10;
+
+    bala->setPosicao(xBala,y);
+    fumaca->setPosicao(x,y);
+    tempo.setTempoOriginal(2+(2*rand()%10));
+    tempo.setUnidade(GBF::Kernel::Timer::TEMPO_SEGUNDO);
+    tempo.setResetar();
+}
+
+Canhao::~Canhao()
+{
+   delete(fumaca);
+   delete(bala);
+}
+
+void Canhao::acao()
+{
+    tempo.processar();
+
+    if(tempo.isTerminou()){
+        xBala-=6;
+        if (xBala<0){
+            xBala=ponto.x-10;
+            fumaca->animacao.setInicio();
+            tempo.setResetar();
+        }
+    }
+}
+
+void Canhao::desenhar()
+{
+    if(tempo.isTerminou()){
+        bala->desenhar(xBala,ponto.y+1);
+        if (!fumaca->animacao.isFim()){
+            fumaca->desenhar(ponto.x-30,ponto.y-20);
+        }
+    }
+}
---
+++ projetos/demos/Gameta_SalaTreino/src/Jogador.h
@@ -0,0 +1,47 @@
+
+#ifndef _CAVEMAN_H
+#define _CAVEMAN_H
+
+
+
+#include <GBF/InputSystem.h>
+
+#include <GBF/SpriteFactory.h>
+#include <GBF/FrameLayer.h>
+#include <GBF/CronometroDecrescente.h>
+#include <GBF/Personagem.h>
+
+enum Estado{
+    PARADO,
+    ANDANDO,
+    RECUO,
+    AVANCO,
+    MORRENDO,
+    PULANDO,
+    CAINDO
+};
+
+struct Altura{
+    int a;
+    int b;
+    int c;
+    int corrente;
+};
+
+//Personagem do Jogo
+class Jogador : public Personagem::Personagem
+{
+  public:
+    Jogador();
+    virtual ~Jogador();
+
+    virtual void acao(GBF::Kernel::Input::InputSystem * input);
+    void desenhar();
+  private:
+
+    Estado estado;
+    std::string getAliasSprite();
+	Altura alturaPulo;
+
+};
+#endif

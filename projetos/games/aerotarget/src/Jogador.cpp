0cc73d9804336281e319d1f6d81611a985298f15
David Ferreira <davidferreira.fz@gmail.com>
2009-08-15 01:26:48 +0000

    Games::AeroTarget, adicionando jogo de avião para tutorial do GBFramework e revista Espirito Livre;

---
+++ projetos/games/aerotarget/src/Jogador.cpp
@@ -0,0 +1,28 @@
+#include "Jogador.h"
+
+Jogador::Jogador()
+{
+    GBF::Imagem::SpriteFactory *spriteFactory = new GBF::Imagem::SpriteFactory("sprites");
+
+    adicionarSpritePrincipal(spriteFactory->criarSpritePersonagem(0,0,59,43,3,6));
+    getSpritePrincipal()->setQtdDirecoes(1);
+    getSpritePrincipal()->animacao.setAutomatico(true);
+
+    delete (spriteFactory);
+
+    setPosicao(320,400);
+}
+
+Jogador::~Jogador()
+{
+    //dtor
+}
+
+void Jogador::acao(GBF::Kernel::Input::InputSystem * input)
+{
+    if (input->teclado->isKey(SDLK_RIGHT)){
+        posicao.x+=4;
+    } else if (input->teclado->isKey(SDLK_LEFT)){
+        posicao.x-=4;
+    }
+}

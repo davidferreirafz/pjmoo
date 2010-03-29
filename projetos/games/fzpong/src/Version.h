1f39af014ba940e685960fa33706bb701354ae68
f-z <f-z@f3efac32-a332-0410-b381-edff871ac65d>
2010-03-29 00:58:25 +0000

    Compatibilizando código com a nova versão do GBFramework;

--- projetos/games/fzpong/src/Version.h
+++ projetos/games/fzpong/src/Version.h
@@ -3,6 +3,12 @@
 
 namespace AutoVersion{
 	
+	//Date Version Types
+	static const char DATE[] = "28";
+	static const char MONTH[] = "03";
+	static const char YEAR[] = "2010";
+	static const double UBUNTU_VERSION_STYLE = 10.03;
+	
 	//Software Status
 	static const char STATUS[] = "FZ";
 	static const char STATUS_SHORT[] = "fz";
@@ -14,7 +20,7 @@ namespace AutoVersion{
 	static const long REVISION = 210;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 78;
+	static const long BUILDS_COUNT = 82;
 	#define RC_FILEVERSION 2,4,37,210
 	#define RC_FILEVERSION_STRING "2, 4, 37, 210\0"
 	static const char FULLVERSION_STRING[] = "2.4.37.210";
